199f4bd2ffb36ab3ce5eea9c83aa228a18df17ba
Manasa Vesala <vesalamanasa@gmail.com>
2019-12-31 01:50:32 +0000

    added comments

--- Stacks/ValidateParaentheses.cs
+++ Stacks/ValidateParaentheses.cs
@@ -3,24 +3,35 @@ using System.Collections;
 
  class ValidatingParaentheses{
   public static void Main(string[] args){
-    Console.WriteLine(validate(""));
-  }
+    Console.WriteLine(validate("()"));
+    Console.WriteLine(validate(null));
 
+  }
+/*Given a string with parentheses, return "true" if string is fully formed;
+ex: input:"()()"
+    output: true
+    input:"))";
+    output:false;
+*/
   private static bool validate(string str){
+    // no reason ;
     Stack s = new Stack();
-      for(int i=0; i<str.Length; i++){
-        if(str[i] == '('){
-          s.Push(str[i]);
-        }
-        if(str[i] == ')'){
-          try{
-            s.Pop();
-          }catch(Exception e){
-            return false;
-          }
+    if(str == null){
+      return false;
+    }
+    for(int i=0; i<str.Length; i++){
+      if(str[i] == '('){
+        s.Push(str[i]);
+      }
+      if(str[i] == ')'){
+        try{
+          s.Pop();
+        }catch(InvalidOperationException){
+          return false;
         }
       }
-
+    }
+  //It says if a stack is empty or not. if count == 0 if returns true because stack is empty.
       return s.Count == 0;
   }
   
